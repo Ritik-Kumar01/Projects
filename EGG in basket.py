b6842a409cda7dbd0546b4355304e030543dbbce
Gourab <professionalkillereye580@gmail.com>
2022-11-15 23:26:20 +0530

    edited EGG in basket.py

--- EGG in basket.py
+++ EGG in basket.py
@@ -9,6 +9,7 @@ def diagonal_sum_check(matrix_list,sumL):
     if rds==sumL and lds==sumL:
         return True
     return False
+
 def row_sum_check(matrix_list,suml):
     s=[]
     for i in matrix_list:
@@ -17,6 +18,7 @@ def row_sum_check(matrix_list,suml):
         if suml!=i:
             return False
     return True
+
 def column_sum_check(matrix_list,suml):
     r=[]
     for i in range(len(matrix_list)):
@@ -28,11 +30,13 @@ def column_sum_check(matrix_list,suml):
         if suml!=i:
             return False
     return True
+
 def magic_matrix_check(matrix_list):
     suml = sum(matrix_list[0])
     if diagonal_sum_check(matrix_list,suml) and row_sum_check(matrix_list,suml) and column_sum_check(matrix_list,suml):
         return True
     return False
+
 def list_matrix_convert(list,order):
     matrix_list=[]
     i=0
@@ -42,6 +46,18 @@ def list_matrix_convert(list,order):
             matrix_list[x].append(list[i])
             i+=1
     return matrix_list
+
+def matrix_convert(n,order):
+    if n%(order**2)==0:
+        num=n//(order**2)
+        matrix=[]
+        for i in range(order):
+            l=[]
+            for i in range(order):
+                l.num(n)
+            matrix.append(l)
+        return matrix
+
 def addperm(x,l):
     return [ l[0:i] + [x] + l[i:]  for i in range(len(l)+1) ]
 
@@ -49,6 +65,8 @@ def perm(l):
     if len(l) == 0:
         return [[]]
     return [x for y in perm(l[1:]) for x in addperm(l[0],y) ]
+
+ 
 if __name__=="__main__":
 
     n=list(map(int,input().split()))
