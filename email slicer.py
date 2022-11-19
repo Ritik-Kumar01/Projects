3f637a70613087516a3ab8301ee4d89bd9730d40
Gourab <professionalkillereye580@gmail.com>
2022-11-19 19:48:27 +0530

    added some documentation

--- email slicer.py
+++ email slicer.py
@@ -1,11 +1,15 @@
-def email_sclicer(s):
-    i=s.index("@")
-    return  s[:i],s[i+1:]
+def email_sclicer(email):
+    """
+    seperates text before "@" and after "@"
+    """
+    i=email.index("@")
+    return  email[:i],email[i+1:]  #returns a tuple containing string before and after "@"
 
 email=input("Enter yout email: ")
-
-if "@" in email:
-    username,domain=email_sclicer(email)
-    print(f"username: {username} and domain: {domain}")
-else:
-    print("enter correct email")
+while True:
+    if "@" in email:
+        username,domain=email_sclicer(email)
+        print(f"username: {username} and domain: {domain.upper()}")
+        break
+    else:
+        print("Enter correct Email")
